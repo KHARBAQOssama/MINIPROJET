c2722bf8a013c127fc6313e4704c4eeae05da9ef
Ossama kharbaq <ossamakharbaq4@gmail.com>
2022-09-15 10:57:09 +0100

    creation des structures race et racecars et les deux fonction print

--- miniprojet.c
+++ miniprojet.c
@@ -4,12 +4,33 @@
 #include <string.h>
 
 // Structures section
+struct Race{//une structure Race qui possède les variables membres
+    int numberOfLaps;
+    int currentLap;
+    char firstPlaceDriverName;
+    char firstPlaceRaceCarColor;
 
+};
+
+struct RaceCar { //une structure RaceCar qui a les variables cars
+    char driverName;
+    char raceCarColor;
+    int totalLapTime;
+};
 // Print functions section
+void printIntro(){  //une fonction  qui imprime un message d'introduction amusant pour la course sur la console
+        printf(" Bienvenue a notre evenement principal, les fans de course numerique !\n");
+        printf("J'espere que tout le monde a pris son gouter car nous allons commencer !");
+}
+
+void printCountDown(){ // une fonction qui imprime un message amusant de compte à rebours pour nos coureurs
+    printf("Coureurs prêts ! Dans...\n5\n4\n3\n2\n1\nCourse !");
+}
 
 // Logic functions section
 
 int main()
 {
+    printIntro();
     srand(time(0));
 };
