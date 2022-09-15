2b8f2c37bee9fa8d1bbb2e3d5cd74a3dbfbd1119
Ossama kharbaq <ossamakharbaq4@gmail.com>
2022-09-15 10:35:47 +0100

    initializing the project

---
+++ miniprojet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+// Structures section
+
+// Print functions section
+
+// Logic functions section
+
+int main()
+{
+    srand(time(0));
+};
