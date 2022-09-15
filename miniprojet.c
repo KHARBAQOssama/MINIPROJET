d24941b0a928d390be5743bb081a9db60efe24fa
Ossama kharbaq <ossamakharbaq4@gmail.com>
2022-09-15 11:33:35 +0100

    creation de une fonction pour calculer le temps d'un tour

--- miniprojet.c
+++ miniprojet.c
@@ -24,13 +24,33 @@ void printIntro(){  //une fonction  qui imprime un message d'introduction amusan
 }
 
 void printCountDown(){ // une fonction qui imprime un message amusant de compte à rebours pour nos coureurs
-    printf("Coureurs prêts ! Dans...\n5\n4\n3\n2\n1\nCourse !");
+    printf("\n\nCoureurs prets ! Dans...\n5\n4\n3\n2\n1\nCourse !");
+}
+
+void printCongratulation(){ //une fonction doit accéder aux variables membres de race et afficher un message de félicitations
+    struct Race race;
+    race.firstPlaceDriverName="OSSAMA";
+    race.firstPlaceRaceCarColor="blue";
+    printf("Felicitons tous %s, dans la voiture de course %s, pour son incroyable performance.\nC'etait vraiment une belle course et bonne nuit a tous !",race.firstPlaceDriverName,race.firstPlaceRaceCarColor);
+
 }
 
 // Logic functions section
+int calculateTimeToCompleteLap(){ //return le moyen de calculer le temps que met une voiture de course pour effectuer un tour.
+    int s;
+    int vitesse=(rand() %3)+1;
+    int acceleration=(rand() %3)+1;
+    int nerves=(rand() %3)+1;
+    s = vitesse+acceleration+nerves;
+    return s;
+}
+
+
 
 int main()
 {
     printIntro();
+    printCountDown();
+//    printf("%d",calculateTimeToCompleteLap());
     srand(time(0));
 };
